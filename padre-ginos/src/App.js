1ddd58494d3ad0b3c879638f055515d53e008be2
Henry Tsai <henry88002605@gmail.com>
2024-12-08 16:37:53 +0800

    01-no-frills-react

---
+++ padre-ginos/src/App.js
@@ -0,0 +1,35 @@
+const Pizza = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("p", {}, props.description),
+    ]);
+}
+
+const App = () => {
+    return React.createElement(
+        "div", 
+        {}, [
+            React.createElement("h1", {}, "Padre Gino's"),
+            React.createElement(Pizza, {
+                name: "The Pepperoni Pizza",
+                description: "Some dope pizze yo",
+            }),
+            React.createElement(Pizza, {
+                name: "Americano Pizza",
+                description: "French fries and hot dogs, wft Italy",
+            }),
+            React.createElement(Pizza, {
+                name: "Chicken Pizza?",
+                description: "Chicken nuggies on your pizza, wft UK",
+            }),
+            React.createElement(Pizza, {
+                name: "Baked Potato Pizza",
+                description: "Unholy potato mash, wft Minnesota",
+            }),
+            React.createElement(Pizza),
+        ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
