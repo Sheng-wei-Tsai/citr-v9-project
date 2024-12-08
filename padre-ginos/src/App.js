4bb91565c9388a48f32beeaff564b9988bd39609
Henry Tsai <henry88002605@gmail.com>
2024-12-08 17:34:59 +0800

    install all config and dependancy

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,35 +1,36 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const Pizza = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("p", {}, props.description),
-    ]);
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div", 
-        {}, [
-            React.createElement("h1", {}, "Padre Gino's"),
-            React.createElement(Pizza, {
-                name: "The Pepperoni Pizza",
-                description: "Some dope pizze yo",
-            }),
-            React.createElement(Pizza, {
-                name: "Americano Pizza",
-                description: "French fries and hot dogs, wft Italy",
-            }),
-            React.createElement(Pizza, {
-                name: "Chicken Pizza?",
-                description: "Chicken nuggies on your pizza, wft UK",
-            }),
-            React.createElement(Pizza, {
-                name: "Baked Potato Pizza",
-                description: "Unholy potato mash, wft Minnesota",
-            }),
-            React.createElement(Pizza),
-        ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Some dope pizze yo",
+    }),
+    React.createElement(Pizza, {
+      name: "Americano Pizza",
+      description: "French fries and hot dogs, wft Italy",
+    }),
+    React.createElement(Pizza, {
+      name: "Chicken Pizza?",
+      description: "Chicken nuggies on your pizza, wft UK",
+    }),
+    React.createElement(Pizza, {
+      name: "Baked Potato Pizza",
+      description: "Unholy potato mash, wft Minnesota",
+    }),
+    React.createElement(Pizza),
+  ]);
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
