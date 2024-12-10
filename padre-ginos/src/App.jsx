c9b2acdd46fb5b900639dc620bbd25e41f8d8951
Henry Tsai <henry88002605@gmail.com>
2024-12-10 12:32:31 +0800

    add custom hooks as pizzaoftheday component

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
-
+import PizzaOfTheDay from "./PizzaOfTheDay";
 const App = () => {
   return (
     <StrictMode>
       <div>
         <h1>Padre Gino's - Order Now</h1>
         <Order />
+        <PizzaOfTheDay />
       </div>
     </StrictMode>
   )
