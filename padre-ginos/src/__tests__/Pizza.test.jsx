62190b58d24228649aea546845443589d27319da
Henry Tsai <henry88002605@gmail.com>
2024-12-13 17:53:28 +0800

    test-driven development on a component

--- padre-ginos/src/__tests__/Pizza.test.jsx
+++ padre-ginos/src/__tests__/Pizza.test.jsx
@@ -1,8 +1,10 @@
-import {render} from '@testing-library/react';
-import {expect, test} from 'vitest';
+import {render, cleanup} from '@testing-library/react';
+import {expect, test, afterEach} from 'vitest';
 import Pizza from '../Pizza';
 
-test("alt test renders on Pizza image", async() => {
+afterEach(cleanup);
+
+test("alt test renders on Pizza image", () => {
     const name = "My Favorite Pizza"
     const src = "https://piscum.phtots/200"
     const screen = render(
@@ -11,4 +13,12 @@ test("alt test renders on Pizza image", async() => {
     const img = screen.getByRole("img");
     expect(img.src).toBe(src);
     expect(img.alt).toBe(name);
+});
+
+test("to have default image if none is provided", () => {
+    const screen = render(
+        <Pizza name="something else" description="super cool pizza" />,
+    );
+    const img = screen.getByRole("img");
+    expect(img.src).not.toBe("");
 })
